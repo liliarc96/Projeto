f47d6f983da669cc09ac7c903ddfebedfd07cc28
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2018-12-10 03:30:04 -0200

    Create projeto.py

---
+++ Parte 3/projeto.py
@@ -0,0 +1,25 @@
+from funcoes import *
+
+comando = input('Deseja gerar um dicionário a partir de um arquivo?\n\n S/N\n\n')
+
+if comando == 's' or comando == 'S':
+    nome_arquivo = input('\nDigite o nome do arquivo: ')
+    while nome_arquivo != 'convidados.txt':
+        nome_arquivo = input('\nArquivo inválido, digite novamente o nome do arquivo: ')
+    dicio = gerar_dicionario(nome_arquivo)
+    print('\nDicionário gerado:')
+    print(dicio)
+
+    comando = input('\nDeseja gerar um PDF com o dicionário?\n\n S/N\n\n')
+    if comando == 's' or comando == 'S':
+        gerar_pdf(dicio)
+        comando = input('\nDeseja enviar o PDF gerado por e-mail?\n\n S/N\n\n')
+        if comando == 's' or comando == 'S':
+            de = input('\nQual o remetente? ')
+            para = input('\nQual o destinatário? ')
+            senha = input('\nDigite sua senha: ')
+            enviar_email(de, para, senha)
+        else:
+            print('Fechando o programa.')
+    else:
+        print('Fechando o programa.')
